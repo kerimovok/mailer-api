5185aa3988f594d6487daef2358597258f2f3b1b
Orkhan Karimov <naxro.ok1@gmail.com>
2024-12-07 02:12:35 +0400

    feat: metrics route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -4,12 +4,16 @@ import (
 	"mailer-api/internal/controllers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
 func SetupRoutes(app *fiber.App) {
 	// API routes group
 	api := app.Group("/api")
 	v1 := api.Group("/v1")
+
+	// Monitor route
+	app.Get("/metrics", monitor.New())
 
 	// Mail routes
 	mail := v1.Group("/mails")
